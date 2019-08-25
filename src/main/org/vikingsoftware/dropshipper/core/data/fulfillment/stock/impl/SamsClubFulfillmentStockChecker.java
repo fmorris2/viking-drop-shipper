5c36e2d48928c240e2829ae3f779fda0993e1f20
Freddy <fred11212@hotmail.com>
2019-08-24 23:19:05 -0400

    Updated SamsClub inventory updater to use requests rather than selenium.

--- src/main/org/vikingsoftware/dropshipper/core/data/fulfillment/stock/impl/SamsClubFulfillmentStockChecker.java
+++ src/main/org/vikingsoftware/dropshipper/core/data/fulfillment/stock/impl/SamsClubFulfillmentStockChecker.java
@@ -1,9 +1,21 @@
 package main.org.vikingsoftware.dropshipper.core.data.fulfillment.stock.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+
+import main.org.vikingsoftware.dropshipper.core.data.fulfillment.listing.FulfillmentListing;
 import main.org.vikingsoftware.dropshipper.core.data.fulfillment.stock.AbstractFulfillmentStockChecker;
+import main.org.vikingsoftware.dropshipper.core.data.marketplace.listing.MarketplaceListing;
+import main.org.vikingsoftware.dropshipper.core.data.sku.SkuInventoryEntry;
+import main.org.vikingsoftware.dropshipper.core.data.sku.SkuMapping;
+import main.org.vikingsoftware.dropshipper.core.data.sku.SkuMappingManager;
 import main.org.vikingsoftware.dropshipper.core.utils.SamsClubMetaDataParser;
 import main.org.vikingsoftware.dropshipper.core.web.DriverSupplier;
 import main.org.vikingsoftware.dropshipper.core.web.samsclub.SamsClubWebDriver;
+import zmq.ZError.IOException;
 
 public class SamsClubFulfillmentStockChecker extends AbstractFulfillmentStockChecker<SamsClubWebDriver> {
 
@@ -22,10 +34,28 @@ public class SamsClubFulfillmentStockChecker extends AbstractFulfillmentStockChe
 
 		return instance;
 	}
-
+	
 	@Override
-	protected int parseItemStock(final SamsClubWebDriver driver) {
-		metaData.parse(driver.getPageSource());
+	protected Collection<SkuInventoryEntry> getStockImpl(MarketplaceListing marketListing, FulfillmentListing fulfillmentListing) {
+		final Collection<SkuInventoryEntry> entries = new ArrayList<>();
+		parseAndAddSkuInventoryEntries(marketListing, fulfillmentListing, entries);
+		return entries;
+	}
+	
+	protected void parseAndAddSkuInventoryEntries(MarketplaceListing marketListing,
+			FulfillmentListing fulfillmentListing, Collection<SkuInventoryEntry> entries) {
+		final List<SkuMapping> mappings = SkuMappingManager.getMappingsForMarketplaceListing(marketListing.id);
+		System.out.println("SKU mappings for marketplace listing " + marketListing.id + ": " + mappings.size());
+		try {
+			final String pageSource = Jsoup.connect(fulfillmentListing.listing_url).get().html();
+			entries.add(new SkuInventoryEntry(null, parseItemStock(pageSource), parseItemPrice(pageSource)));
+		} catch(final Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private int parseItemStock(final String pageSource) {
+		metaData.parse(pageSource);
 		
 		//TODO ENSURE TITLE MATCHES EXPECTED FULFILLMENT LISTING TITLE
 		if(!metaData.passesAllListingConditions()) {
@@ -35,6 +65,14 @@ public class SamsClubFulfillmentStockChecker extends AbstractFulfillmentStockChe
 		
 		return metaData.getStock();
 	}
+	
+	private double parseItemPrice(final String pageSource) {
+		metaData.parse(pageSource);
+		return metaData.getPrice();
+	}
+
+	@Override
+	protected int parseItemStock(final SamsClubWebDriver driver) { return -1; }
 
 	@Override
 	protected Class<? extends DriverSupplier<?>> getDriverSupplierClass() {
@@ -42,8 +80,5 @@ public class SamsClubFulfillmentStockChecker extends AbstractFulfillmentStockChe
 	}
 
 	@Override
-	protected double parseItemPrice(SamsClubWebDriver driver) {
-		metaData.parse(driver.getPageSource());
-		return metaData.getPrice();
-	}
+	protected double parseItemPrice(SamsClubWebDriver driver) { return -1.0; }
 }
