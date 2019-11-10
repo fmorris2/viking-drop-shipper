1ea93543f1bd21cc70062bee76e7fa8684375a3b
Freddy <fred11212@hotmail.com>
2019-11-10 11:39:40 -0500

    Margin adjuster improvements. We now increase margin for 'productive listings' as well

--- src/main/org/vikingsoftware/dropshipper/VSDropShipper.java
+++ src/main/org/vikingsoftware/dropshipper/VSDropShipper.java
@@ -8,6 +8,7 @@ import main.org.vikingsoftware.dropshipper.order.executor.OrderExecutor;
 import main.org.vikingsoftware.dropshipper.order.parser.OrderParser;
 import main.org.vikingsoftware.dropshipper.order.tracking.OrderTracking;
 import main.org.vikingsoftware.dropshipper.order.tracking.history.TrackingHistoryUpdater;
+import main.org.vikingsoftware.dropshipper.pricing.margins.MarginAdjuster;
 
 public class VSDropShipper {
 
@@ -16,10 +17,10 @@ public class VSDropShipper {
 	private static final CycleParticipant[] MODULES = {
 		new OrderParser(),
 		new OrderExecutor(),
-		//new MarginAdjuster(),
+		new MarginAdjuster(),
 		new InventoryUpdater(),
 		new OrderTracking(),
-		new TrackingHistoryUpdater()
+		new TrackingHistoryUpdater(),
 	};
 
 	public static void main(final String[] args) throws InterruptedException {
--- src/main/org/vikingsoftware/dropshipper/pricing/margins/MarginAdjuster.java
+++ src/main/org/vikingsoftware/dropshipper/pricing/margins/MarginAdjuster.java
@@ -22,8 +22,14 @@ public class MarginAdjuster implements CycleParticipant {
 	 */
 	private static final long LAST_ORDER_TIME_WINDOW = ((60_000 * 60) * 24) * 7;
 	
+	/*
+	 * If a marketplace listing has sold this many orders in the LAST_ORDER_TIME_WINDOW,
+	 * we'll consider it a productive listing
+	 */
+	private static final int MIN_PRODUCTIVE_ORDERS_IN_LAST_ORDER_TIME_WINDOW = 2;
+	
 	private static final double MARGIN_FLOOR = 5.0; //5% margin is the lowest we'll go
-	private static final double MARGIN_ADJUSTMENT_STEP = 1.0; //We'll lower the margin by this amount on each adjustment
+	private static final double MARGIN_ADJUSTMENT_STEP = 1.0; //We'll change the margin by this amount on each adjustment
 	
 	private static final String STAGNANT_LISTING_QUERY = "SELECT marketplace_listing.id as stagnantMarketplaceListingID, marketplace_listing.target_margin as currentMargin, marketplace_listing.listing_title as listingTitle" + 
 			" FROM marketplace_listing" + 
@@ -35,40 +41,72 @@ public class MarginAdjuster implements CycleParticipant {
 			" GROUP BY processed_order.fulfillment_listing_id" + 
 			" ORDER BY date_processed DESC) AS t2" + 
 			") ON fulfillment_mapping.fulfillment_listing_id = t2.fulfillment_listing_id" + 
-			" WHERE marketplace_listing.active" + 
-			" AND (UNIX_TIMESTAMP() * 1000) - marketplace_listing.last_margin_update > " + LAST_MARGIN_UPDATE_TIME_WINDOW;
+			" WHERE (UNIX_TIMESTAMP() * 1000) - marketplace_listing.last_margin_update > " + LAST_MARGIN_UPDATE_TIME_WINDOW;
+	
+	private static final String PRODUCTIVE_LISTING_QUERY = "SELECT * FROM (SELECT marketplace_listing.id AS productiveMarketplaceListingID, marketplace_listing.target_margin AS currentMargin," + 
+			" marketplace_listing.listing_title AS listingTitle, COUNT(marketplace_listing.id) AS num_orders" + 
+			" FROM marketplace_listing" + 
+			" INNER JOIN customer_order ON customer_order.marketplace_listing_id=marketplace_listing.id" + 
+			" WHERE (UNIX_TIMESTAMP() * 1000) - customer_order.date_parsed <= " + LAST_ORDER_TIME_WINDOW +
+			" AND (UNIX_TIMESTAMP() * 1000) - marketplace_listing.last_margin_update > " + LAST_MARGIN_UPDATE_TIME_WINDOW +
+			" GROUP BY marketplace_listing.id) sub_query WHERE num_orders >= " + MIN_PRODUCTIVE_ORDERS_IN_LAST_ORDER_TIME_WINDOW;
 	
 	private boolean debug = true;
 	
 	public static void main(final String[] args) {
-		System.out.println(STAGNANT_LISTING_QUERY);
+		//System.out.println(STAGNANT_LISTING_QUERY);
+		//System.out.println(PRODUCTIVE_LISTING_QUERY);
+		new MarginAdjuster().cycle();
 	}
 
 	@Override
 	public void cycle() {
 		debug("Cycling...");
 		
-		final List<StagnantListing> stagnantListings = getStagnantListings();
+		final List<ListingDueForMarginAdjustment> stagnantListings = getStagnantListings();
 		debug("There are currently " + stagnantListings.size() + " stagnant listings to adjust...");
 		
-		for(final StagnantListing listing : stagnantListings) {
+		final List<ListingDueForMarginAdjustment> productiveListings = getProductiveListings();
+		debug("There are currently " + productiveListings.size() + " productive listings to adjust...");
+		
+		final List<ListingDueForMarginAdjustment> listingsToAdjust = new ArrayList<>();
+		listingsToAdjust.addAll(stagnantListings);
+		listingsToAdjust.addAll(productiveListings);
+		
+		for(final ListingDueForMarginAdjustment listing : listingsToAdjust) {
 			adjustListing(listing);
 		}
+		
+		
 	}
 	
 	/*
 	 * A Listing is considered "Stagnant" if the following conditions are true:
-	 *    - The listing is active
 	 *    - The last margin update for the listing is > MARGIN_UPDATE_TIME_WINDOW
 	 *    - The last order for the listing is > LAST_ORDER_TIME_WINDOW   	
 	 */
-	private final List<StagnantListing> getStagnantListings() {
-		final List<StagnantListing> stagnantListings = new ArrayList<>();
+	private final List<ListingDueForMarginAdjustment> getStagnantListings() {
+		final List<ListingDueForMarginAdjustment> stagnantListings = new ArrayList<>();
 		try (final Statement st = VSDSDBManager.get().createStatement();
 			 final ResultSet res = st.executeQuery(STAGNANT_LISTING_QUERY)) {
 			while(res.next()) {
-				final StagnantListing listing = new StagnantListing(res.getInt("stagnantMarketplaceListingID"),
-						res.getDouble("currentMargin"), res.getString("listingTitle"));
+				final ListingDueForMarginAdjustment listing = new ListingDueForMarginAdjustment(res.getInt("stagnantMarketplaceListingID"),
+						res.getDouble("currentMargin"), res.getString("listingTitle"), MarginAdjustmentType.STAGNANT);
+				stagnantListings.add(listing);
+			}
+		} catch(final Exception e) {
+			e.printStackTrace();
+		}
+		return stagnantListings;
+	}
+	
+	private final List<ListingDueForMarginAdjustment> getProductiveListings() {
+		final List<ListingDueForMarginAdjustment> stagnantListings = new ArrayList<>();
+		try (final Statement st = VSDSDBManager.get().createStatement();
+			 final ResultSet res = st.executeQuery(PRODUCTIVE_LISTING_QUERY)) {
+			while(res.next()) {
+				final ListingDueForMarginAdjustment listing = new ListingDueForMarginAdjustment(res.getInt("productiveMarketplaceListingID"),
+						res.getDouble("currentMargin"), res.getString("listingTitle"), MarginAdjustmentType.PRODUCTIVE);
 				stagnantListings.add(listing);
 			}
 		} catch(final Exception e) {
@@ -84,17 +122,23 @@ public class MarginAdjuster implements CycleParticipant {
 	 *    ELSE
 	 *       FLAG LISTING FOR PURGE EXAMINATION      
 	 */
-	private final void adjustListing(final StagnantListing listing) {
+	private final void adjustListing(final ListingDueForMarginAdjustment listing) {
 		debug("Starting adjustment checks for: " + listing);
-		if(listing.currentMargin > MARGIN_FLOOR) {
-			decreaseMargin(listing);
-		} else { //flag listing for purge examination
-			flagForPurgeExamination(listing);
+		if(listing.adjustmentType == MarginAdjustmentType.STAGNANT) {
+			if(listing.currentMargin > MARGIN_FLOOR) {
+				adjustMargin(listing);
+			} else { //flag listing for purge examination
+				flagForPurgeExamination(listing);
+			}
+		} else { //productive listing
+			adjustMargin(listing);
 		}
 	}
 	
-	private void decreaseMargin(final StagnantListing listing) {
-		final double newMargin = Math.max(MARGIN_FLOOR, listing.currentMargin - MARGIN_ADJUSTMENT_STEP);
+	private void adjustMargin(final ListingDueForMarginAdjustment listing) {
+		final double newMargin = listing.adjustmentType == MarginAdjustmentType.STAGNANT 
+				? Math.max(MARGIN_FLOOR, listing.currentMargin - MARGIN_ADJUSTMENT_STEP)
+				: listing.currentMargin + MARGIN_ADJUSTMENT_STEP ;
 		debug("Adjusting margin from " + listing.currentMargin + "% --> " + newMargin + "% for: " + listing);
 		try (final Statement st = VSDSDBManager.get().createStatement()) {
 			final long ms = System.currentTimeMillis();
@@ -107,7 +151,7 @@ public class MarginAdjuster implements CycleParticipant {
 		}
 	}
 	
-	private void flagForPurgeExamination(final StagnantListing listing) {
+	private void flagForPurgeExamination(final ListingDueForMarginAdjustment listing) {
 		debug("Flagging for purge examination: " + listing);
 		try (final Statement st = VSDSDBManager.get().createStatement()) {
 			st.execute("UPDATE marketplace_listing SET needs_purge_examination=1"
