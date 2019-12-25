30ed1630261ec8123fe272b92c64c2c5d23f07f5
Freddy <fred11212@hotmail.com>
2019-12-24 23:27:18 -0500

    Removed concurrent modules.

--- src/main/org/vikingsoftware/dropshipper/VSDropShipper.java
+++ src/main/org/vikingsoftware/dropshipper/VSDropShipper.java
@@ -1,14 +1,10 @@
 package main.org.vikingsoftware.dropshipper;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 import main.org.vikingsoftware.dropshipper.core.CycleParticipant;
 import main.org.vikingsoftware.dropshipper.core.browser.BrowserRepository;
 import main.org.vikingsoftware.dropshipper.core.ebay.EbayAccountActivityFees;
 import main.org.vikingsoftware.dropshipper.core.web.LoginWebDriver;
 import main.org.vikingsoftware.dropshipper.inventory.InventoryUpdater;
-import main.org.vikingsoftware.dropshipper.order.executor.OrderExecutor;
 import main.org.vikingsoftware.dropshipper.order.parser.OrderParser;
 import main.org.vikingsoftware.dropshipper.order.tracking.OrderTracking;
 import main.org.vikingsoftware.dropshipper.order.tracking.history.TrackingHistoryUpdater;
@@ -18,11 +14,9 @@ public class VSDropShipper {
 
 	public static final String VS_PHONE_NUM = "9162450125";
 	
-	private static final long ORDER_EXECUTOR_CYCLE_TIME = 60_000 * 2;
-	private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
-
 	private static final CycleParticipant[] MAIN_THREAD_MODULES = {
 		new OrderParser(),
+		new InventoryUpdater(),
 		new MarginAdjuster(),
 		new InventoryUpdater(),
 		new OrderTracking(),
@@ -31,36 +25,7 @@ public class VSDropShipper {
 	};
 
 	public static void main(final String[] args) throws InterruptedException {
-		startConcurrentModules();
 		cycle();
-	}
-	
-	private static void startConcurrentModules() {
-		startOrderExecutionModule();
-	}
-	
-	private static void startOrderExecutionModule() {
-		final CycleParticipant orderExecutor = new OrderExecutor();
-		Executors.newSingleThreadExecutor().execute(() -> {
-			while(true) {
-				lock.writeLock().lock();
-				try {
-					if(orderExecutor.shouldCycle()) {
-						orderExecutor.cycle();
-					}
-				} catch(final Exception e) {
-					e.printStackTrace();
-				} finally {
-					lock.writeLock().unlock();
-				}
-				
-				try {
-					Thread.sleep(ORDER_EXECUTOR_CYCLE_TIME);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
 	}
 	
 	private static void cycle() {
@@ -79,18 +44,12 @@ public class VSDropShipper {
 						System.out.println("Failed to execute module: " + module);
 					}
 				}
-	
-				try {
-					lock.writeLock().lock();
-					BrowserRepository.get().replaceAll();
-					LoginWebDriver.clearSessionCaches();
-					Runtime.getRuntime().exec("pkill -9 firefox");
-					Runtime.getRuntime().exec("pkill -9 geckodriver");
-				} catch(final Exception e) {
-					e.printStackTrace();
-				} finally {
-					lock.writeLock().unlock();
-				}
+				
+				BrowserRepository.get().replaceAll();
+				LoginWebDriver.clearSessionCaches();
+				Runtime.getRuntime().exec("pkill -9 firefox");
+				Runtime.getRuntime().exec("pkill -9 geckodriver");
+				
 			} catch(final Exception e) {
 				e.printStackTrace();
 			}
